4b393c6a42cdb9c49f70cb37219121cf69635768
osamagucCitySoc <osama.cs.eng@gmail.com>
2014-09-01 12:56:51 +0400

    1- Added the “CONSTANTS” file that will be hold to hold all constants, to keep consistency across the whole app.

---
+++ ColourMemory/CONSTANTS.h
@@ -0,0 +1,15 @@
+//
+//  CONSTANTS.h
+//  ColourMemory
+//
+//  Created by OsamaMac on 9/1/14.
+//  Copyright (c) 2014 Osama Rabie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CONSTANTS : NSObject
+
+#define firstTimeKey "firstTimeKey"
+
+@end
---
+++ ColourMemory/CONSTANTS.m
@@ -0,0 +1,13 @@
+//
+//  CONSTANTS.m
+//  ColourMemory
+//
+//  Created by OsamaMac on 9/1/14.
+//  Copyright (c) 2014 Osama Rabie. All rights reserved.
+//
+
+#import "CONSTANTS.h"
+
+@implementation CONSTANTS
+
+@end
