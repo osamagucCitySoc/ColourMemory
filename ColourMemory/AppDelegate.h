53a9efeadf7a468c4ae651c6c16b4d30d48e361f
osamagucCitySoc <osama.cs.eng@gmail.com>
2014-09-01 11:34:19 +0400

    Initial Commit

---
+++ ColourMemory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColourMemory
+//
+//  Created by OsamaMac on 9/1/14.
+//  Copyright (c) 2014 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColourMemoryTests/ColourMemoryTests.m
@@ -0,0 +1,34 @@
+//
+//  ColourMemoryTests.m
+//  ColourMemoryTests
+//
+//  Created by OsamaMac on 9/1/14.
+//  Copyright (c) 2014 Osama Rabie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ColourMemoryTests : XCTestCase
+
+@end
+
+@implementation ColourMemoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
